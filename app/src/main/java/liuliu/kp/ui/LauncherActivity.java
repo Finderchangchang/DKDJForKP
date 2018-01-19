2aacd948269b939a008f0e8b8f82593e382cbecb
1031066280@qq.com <chang19930228>
2018-01-19 18:05:32 +0800

    666

---
+++ app/src/main/java/liuliu/kp/ui/LauncherActivity.java
@@ -0,0 +1,20 @@
+package liuliu.kp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import liuliu.kp.R;
+import liuliu.kp.base.BaseActivity;
+
+public class LauncherActivity extends BaseActivity {
+
+    @Override
+    public void initViews() {
+        setContentView(R.layout.activity_launcher);
+    }
+
+    @Override
+    public void initEvents() {
+
+    }
+}
